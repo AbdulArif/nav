13c286dac170fa3abc16ed2bc9e0340f1b677fd0
abdularif <abdularif.dgp@gmail.com>
2022-03-25 13:51:04 +0530

    update

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,10 +3,12 @@ import { NativeScriptCommonModule } from '@nativescript/angular'
 
 import { HomeRoutingModule } from './home-routing.module'
 import { HomeComponent } from './home.component'
+import { ReactiveFormsModule } from "@angular/forms";
+
 
 @NgModule({
-  imports: [NativeScriptCommonModule, HomeRoutingModule],
+  imports: [NativeScriptCommonModule, HomeRoutingModule, ReactiveFormsModule],
   declarations: [HomeComponent],
   schemas: [NO_ERRORS_SCHEMA],
 })
-export class HomeModule {}
+export class HomeModule { }
